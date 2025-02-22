862ef91b3f408ebe99e98775f7d553e1d3437be7
Siraphat-ohm <siraphat975@gmail.com>
2025-02-22 21:09:54 +0700

    const.py

---
+++ const.py
@@ -0,0 +1,10 @@
+JOYSTICKS = {
+    1: {"x_pin": 13, "y_pin": 12, "sw_pin": 14},  # Joystick 1
+    2: {"x_pin": 21, "y_pin": 22, "sw_pin": 23},  # Joystick 2
+    3: {"x_pin": 25, "y_pin": 26, "sw_pin": 27},  # Joystick 3
+}
+
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 64
+
+I2C_MUTIPLEXER = 0x70
