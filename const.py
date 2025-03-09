90f35d4cee52e48c69a302bb516334c89cf7efb4
Siraphat-ohm <siraphat975@gmail.com>
2025-03-09 15:34:11 +0700

    [+] modified player and joystick pin

--- const.py
+++ const.py
@@ -1,8 +1,8 @@
 PLAYER_CONFIG = [
-    {"joystick_pins": (0,1,3), "channel": 0},
-    {"joystick_pins": (2,3,17), "channel": 1},
-    {"joystick_pins": (4,5,7), "channel": 2},
-    {"joystick_pins": (6,7,5), "channel": 3},
+    {"joystick_pins": (2, 3, 17), "channel": 1},
+    {"joystick_pins": (4, 5, 7), "channel": 2},
+    {"joystick_pins": (6, 7, 12), "channel": 3},
+    {"joystick_pins": (0, 1, 3), "channel": 4},
 ]
 
 I2C_ADDR = 0x70
